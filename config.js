a22c12fde68f0ba56810164ca759feb8d8ea8ff2
C. M. Lin <bagilu@gmail.com>
2026-03-31 10:11:53 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// 請將本檔複製為 config.js，並填入您自己的 Supabase 專案資訊。
+// GitHub Pages 前端只能使用 anon key，請勿放入 service_role key。
+
+window.SUPABASE_URL = 'https://mfljkyvdadxlrbxlboce.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs';
